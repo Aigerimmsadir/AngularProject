aa867909942557a588a53c8263ee7718b0cd0ebd
unknown <aigerimsadir@gmail.com>
2019-10-16 19:07:29 +0600

    tried to add routes

--- prep_project/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-admin.site.register(Company)
-admin.site.register(Department)
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Report)
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from "./auth-routing.module";
 
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
+    AppRoutingModule,
     CommonModule,
     ReactiveFormsModule,
   ]
